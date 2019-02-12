fe86cd8f5a58268da8e36142d5c2ff9eddd5181c
Enrico Bazzicalupo <enricobazzical@gmail.com>
2019-02-12 10:53:34 +0100

    Tested Loop ("TRY"), fixed few issues and launched

--- TRY-MACROGEN-mapping-noLL112.sh
+++ TRY-MACROGEN-mapping-noLL112.sh
@@ -35,19 +35,35 @@
 ## VARIABLE and PATHS definition ##
 ###################################
 
-# List of all Lynx lynx sample codes in MACROGEN project:
+# List of all Lynx lynx sample codes in MACROGEN project - trial version - :
 # (HERE I removed LL112 as the analysis for that sample were already run during testing)
 MacroGenARRAY=($(ls /backup/grupolince/raw_data/MACROGEN/MACROGEN_trimmed/*.fastq.gz | rev | cut -d'/' -f 1 | rev | cut -d '_' -f1 | uniq | grep -v LL112))
 # Path to cat reference genome:
 REF=/home/GRUPOS/grupolince/reference_genomes/felis_catus_genome/Felis_catus.Felis_catus_9.0.dna.toplevel.fa
 # No. of computer cores used. 20 = OK, >20 = ask people first!
 THREADS=10
-# Path to output files, were BAMS are generated:
-OUT=/home/ebazzicalupo/CatRef_bams
-# path to MACROGEN fastq files:
-MacroGenPATH=/backup/grupolince/raw_data/MACROGEN/MACROGEN_trimmed/
-# BARCODE MACROGEN, where the ID given by the sequencing company is converted to our sample ID
+# Path to output files, were BAMS are generated - trial version - :
+OUT=/home/ebazzicalupo/try_map_loop
+# path to MACROGEN fastq files - trial version - :
+MacroGenPATH=/home/ebazzicalupo/try_fastqs
+# BARCODE MACROGEN, where the ID given by the sequencing company is converted to our sample ID - trial version -
 declare -A BARCODEID=(["LC1"]="c_lc_zz_0001" ["LL112"]="c_ll_vl_0112" ["LL146"]="c_ll_ya_0146" ["LL212"]="c_ll_cr_0212" ["LL90"]="c_ll_ki_0090" ["LR1"]="c_lr_zz_0001")
+
+
+## This version is to TRY with FASTQs of 10000 reads ONLY ##
+# therefore it will have different input fastqs which have been generated like this:
+
+# generate FASTQs:
+# for i in ${MacroGenARRAY[@]}
+#   do
+#     echo ${i}
+#     zcat /backup/grupolince/raw_data/MACROGEN/MACROGEN_trimmed/${i}_R1_trimmed.fastq.gz | \
+#     head -40000 > /home/ebazzicalupo/try_fastqs/${i}_R1_trimmed_10kr.fastq
+#     bgzip /home/ebazzicalupo/try_fastqs/${i}_R1_trimmed_10kr.fastq
+#     zcat /backup/grupolince/raw_data/MACROGEN/MACROGEN_trimmed/${i}_R2_trimmed.fastq.gz | \
+#     head -40000 > /home/ebazzicalupo/try_fastqs/${i}_R2_trimmed_10kr.fastq
+#     bgzip /home/ebazzicalupo/try_fastqs/${i}_R2_trimmed_10kr.fastq
+# done
 
 ##########################
 ## Mapping with BWA MEM ##
@@ -56,14 +72,14 @@ declare -A BARCODEID=(["LC1"]="c_lc_zz_0001" ["LL112"]="c_ll_vl_0112" ["LL146"]=
 for i in ${MacroGenARRAY[@]}
   do
 
-    echo " *********** \n\n - Mapping ${i} - \n\n ***********"
-    bwa mem $REF $MacroGenPATH/${i}_R1_trimmed.fastq.gz $MacroGenPATH/${i}_R2_trimmed.fastq.gz \
+    echo " - Mapping ${i} -"
+    bwa mem $REF $MacroGenPATH/${i}_R1_trimmed_10kr.fastq.gz $MacroGenPATH/${i}_R2_trimmed_10kr.fastq.gz \
     -t $THREADS | samtools view -hbS -@ $THREADS - -o $OUT/${i}.cat_ref.bam
     echo " - Sorting ${i} -"
     samtools sort -@ $THREADS $OUT/${i}.cat_ref.bam -o $OUT/${i}.cat_ref.sorted.bam \
     && rm $OUT/${i}.cat_ref.bam
 
-    echo " *********** \n\n - Adding READ Groups to ${i} and changing name to ${BARCODEID["${i}"]} - \n\n ***********"
+    echo " - Adding READ Groups to ${i} and changing name to ${BARCODEID["${i}"]} -"
     run=($(echo $i | cut -d"_" -f 1))  #Extracting run from i
     echo $run
     java -jar /opt/picard-tools/picard.jar AddOrReplaceReadGroups \
@@ -73,10 +89,10 @@ for i in ${MacroGenARRAY[@]}
     RGPL=Illumina RGPU=${run} RGSM=${BARCODEID["${i}"]} \
     VALIDATION_STRINGENCY=SILENT && rm $OUT/${i}.cat_ref.sorted.bam
 
-    echo " *********** \n\n - Marking Duplicates of ${BARCODEID["${i}"]} and Re-Sorting - \n\n ***********"
+    echo " - Marking Duplicates of ${BARCODEID["${i}"]} and Re-Sorting -"
 
     java -jar /opt/picard-tools/picard.jar MarkDuplicates \
-    METRICS_FILE=$OUT/${i}_rmdup.txt \
+    METRICS_FILE=${i}_rmdup.txt \
     I=$OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg.bam \
     O=$OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup.bam \
     MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=800
@@ -86,7 +102,7 @@ for i in ${MacroGenARRAY[@]}
     rm $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup.bam
     samtools index $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted.bam
 
-    echo " *********** \n\n - Realigning ${BARCODEID["${i}"]} - \n\n ***********"
+    echo " - Realigning ${BARCODEID["${i}"]} -"
 
     # RealignerTargetCreator
     java -jar /home/tmp/Software/GATK_3.4/GenomeAnalysisTK.jar -T RealignerTargetCreator \
@@ -98,7 +114,7 @@ for i in ${MacroGenARRAY[@]}
     -I $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted.bam \
     -o $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam
     rm $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted.bam
-    samtools flagstat $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam \
+    samtools flagstat $OUT/${BARCODEID["${i}"]}_sorted_rmdup_sorted_indelrealigner.bam \
     > $OUT/${BARCODEID["${i}"]}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.stats
 
 done
