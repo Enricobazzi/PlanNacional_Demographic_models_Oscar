e14ae8cc17f878b1e95af2f8d68e8c4fa01d3ab4
Enrico Bazzicalupo <enricobazzicalupo@MacBook-Pro-de-Enrico.local>
2019-03-29 14:36:41 +0100

    Launched BOBCAT1. Script for CANDILES completed and launched.

--- CANDILES-mapping.sh
+++ CANDILES-mapping.sh
@@ -34,3 +34,98 @@
 # samtools faidx /home/GRUPOS/grupolince/reference_genomes/felis_catus_genome/Felis_catus.Felis_catus_9.0.dna.toplevel.fa
 #
 # java -jar /opt/picard-tools/picard.jar CreateSequenceDictionary R= /home/GRUPOS/grupolince/reference_genomes/felis_catus_genome/Felis_catus.Felis_catus_9.0.dna.toplevel.fa O= /home/GRUPOS/grupolince/reference_genomes/felis_catus_genome/Felis_catus.Felis_catus_9.0.dna.toplevel.dict
+
+###################################
+## VARIABLE and PATHS definition ##
+###################################
+
+# List of all Lynx pardinus sample codes in Project Genoma part2:
+CandilesARRAY=($(ls /home/ebazzicalupo/fastqs/Enrico_moveLypa/*.fastq.gz | rev | cut -d'/' -f 1 | rev | cut -d '_' -f1,2,3,4 | uniq))
+# Path to cat reference genome:
+REF=/home/GRUPOS/grupolince/reference_genomes/felis_catus_genome/Felis_catus.Felis_catus_9.0.dna.toplevel.fa
+# No. of computer cores used. 20 = OK, >20 = ask people first!
+THREADS=10
+# Path to output files, were BAMS are generated:
+OUT=/home/ebazzicalupo/CatRef_bams
+# path to Project Genoma 2 fastq files:
+CandilesPATH=/home/ebazzicalupo/fastqs/Enrico_moveLypa
+# BARCODE CANDILES copied from Maria:
+declare -A BARCODEID=(["6220RAAXX_lane3_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane4_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane6_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane7_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane8_sequence_0"]="c_lp_sm_0221" ["62AHEAAXX_lane1_sequence_0"]="c_lp_sm_0221" ["621CYAAXX_lane1_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane1_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane5_sequence_0"]="c_lp_sm_0221" ["6220RAAXX_lane2_sequence_0"]="c_lp_sm_0221")
+
+##########################
+## Mapping with BWA MEM ##
+##########################
+
+for i in ${CandilesARRAY[@]}
+  do
+
+    echo -e " ********************** \n\n - Mapping ${i} - \n\n **********************"
+    bwa mem $REF $CandilesPATH/${i}_1.fastq.gz $CandilesPATH/${i}_2.fastq.gz \
+    -t $THREADS | samtools view -hbS -@ $THREADS - -o $OUT/${i}.cat_ref.bam
+    echo " - Sorting ${i} -"
+    samtools sort -@ $THREADS $OUT/${i}.cat_ref.bam -o $OUT/${i}.cat_ref.sorted.bam \
+    && rm $OUT/${i}.cat_ref.bam
+
+    echo -e " ********************** \n\n - Adding READ Groups to ${i} and changing name to ${BARCODEID["${i}"]} - \n\n **********************"
+    run=($(echo $i | cut -d"_" -f 1))  #Extracting run from i
+    echo $run
+    java -jar /opt/picard-tools/picard.jar AddOrReplaceReadGroups \
+    I=$OUT/${i}.cat_ref.sorted.bam \
+    O=$OUT/${BARCODEID["${i}"]}_${i}_cat_ref_sorted_rg.bam \
+    RGID=${i} RGLB=${BARCODEID["${i}"]}_lib \
+    RGPL=Illumina RGPU=${run} RGSM=${BARCODEID["${i}"]} \
+    VALIDATION_STRINGENCY=SILENT && rm $OUT/${i}.cat_ref.sorted.bam
+
+done
+
+SampleARRAY=($(for i in ${CandilesARRAY[@]}; do echo ${BARCODEID["${i}"]}; done | sort -u))
+
+
+for i in ${SampleARRAY[@]}
+  do
+
+    echo -e " ********************** \n\n - Merging all ${i} samples and Re-Sorting - \n\n **********************"
+
+    ls $OUT/${i}_*_sorted_rg.bam  > $OUT/${i}.bam.list
+    samtools merge  -@ $THREADS -r $OUT/${i}_merged.bam -b $OUT/"${i}".bam.list
+
+    BAMARRAY=($(cat $OUT/${i}.bam.list))
+    for k in ${BAMARRAY[@]}
+      do
+        echo "Removing $k"
+        rm $k
+    done
+
+    echo " Re-Sorting ${i}"
+    samtools sort  -@ $THREADS $OUT/${i}_merged.bam -o $OUT/${i}_cat_ref_sorted_rg.bam && rm $OUT/${i}_merged.bam;
+
+
+    echo -e " ********************** \n\n - Marking Duplicates of ${i} and Re-Sorting - \n\n **********************"
+
+    java -jar /opt/picard-tools/picard.jar MarkDuplicates \
+    METRICS_FILE=$OUT/${i}_rmdup.txt \
+    I=$OUT/${i}_cat_ref_sorted_rg.bam \
+    O=$OUT/${i}_cat_ref_sorted_rg_rmdup.bam \
+    MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=800
+    rm $OUT/${i}_cat_ref_sorted_rg.bam
+    samtools sort $OUT/${i}_cat_ref_sorted_rg_rmdup.bam \
+    -@ $THREADS -o $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted.bam
+    rm $OUT/${i}_cat_ref_sorted_rg_rmdup.bam
+    samtools index $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted.bam
+
+    echo -e " ********************** \n\n - Realigning ${i} - \n\n **********************"
+
+    # RealignerTargetCreator - ADDED -fixMisencodedQuals because these reads are from Illumina 1.5
+    java -jar /home/tmp/Software/GATK_3.4/GenomeAnalysisTK.jar -T RealignerTargetCreator \
+    -nt $THREADS -R $REF -fixMisencodedQuals -I $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted.bam \
+    -o $OUT/${i}_realignertargetcreator.intervals
+    # IndelRealigner
+    java -jar /home/tmp/Software/GATK_3.4/GenomeAnalysisTK.jar -T IndelRealigner \
+    -R $REF -targetIntervals $OUT/${i}_realignertargetcreator.intervals \
+    -I $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted.bam \
+    -o $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam
+    rm $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted.bam
+    samtools flagstat $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam \
+    > $OUT/${i}_cat_ref_sorted_rg_rmdup_sorted_indelrealigner.stats
+
+done
