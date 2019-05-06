51d8453f303f230d645c34e152b9e8954c35e132
Enrico Bazzicalupo <enricobazzicalupo@MacBook-Pro-de-Enrico.local>
2019-05-06 13:13:46 +0200

    small changes in explanation

--- 1.Calling-executables/chr_parallel_caller.sh
+++ 1.Calling-executables/chr_parallel_caller.sh
@@ -5,38 +5,40 @@
 ############################
 
 # This program will create a screen and execute the sample_chr_Haplotypecaller.sh
-# script for each sample in an array of samples, for a single chromosome defined by
+# script for each sample in an array of sample BAMs, for a single chromosome defined by
 # an element of the array of chromosome BEDs. The BED array element number must be defined
 # while launching the script as such:
 
 # ./chr_parallel_caller.sh <N>
 
-# where N is the BED array element number. Here is the list of all elements in the array
+# where N is the BED array element number. Here is the list of all elements in the BED array
 # with their corresponding number:
 
-# 1 = A1
-# 2 = A2
-# 3 = A3
-# 4 = B1
-# 5 = B2
-# 6 = B3
-# 7 = B4
-# 8 = C1
-# 9 = C2
-# 10 = D1
-# 11 = D2
-# 12 = D3
-# 13 = D4
-# 14 = E1
-# 15 = E2
-# 16 = E3
-# 17 = F1
-# 18 = F2
-# 19 = MT
-# 20 = rest
-# 21 = X
+# 0 = A1
+# 1 = A2
+# 2 = A3
+# 3 = B1
+# 4 = B2
+# 5 = B3
+# 6 = B4
+# 7 = C1
+# 8 = C2
+# 9 = D1
+# 10 = D2
+# 11 = D3
+# 12 = D4
+# 13 = E1
+# 14 = E2
+# 15 = E3
+# 16 = F1
+# 17 = F2
+# 18 = MT
+# 19 = rest
+# 20 = X
 
 # More information on the BEDs and the script can be found in the 1.Parallel_Calling_pipeline.md file
+
+# CAREFUL: sample_chr_Haplotypecaller.sh MUST be in the same folder where this is launched!!!
 
 
 ###################################
